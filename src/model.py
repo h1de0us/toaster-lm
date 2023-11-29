Fix trainloop, add gradient clipping, logging and saving checkpoints

src/model.py
@@ -1,9 +1,9 @@
 import torch
 from torch import nn
 from torch.utils.data import Dataset
-from torch import functional as F
+from torch.nn import functional as F
 
-from dataset import TextDataset
+from src.dataset import TextDataset
 
 
 class PositionalEncoding(nn.Module):
@@ -29,7 +29,13 @@ class PositionalEncoding(nn.Module):
     def forward(self, x):
         x = x + self.pe[:, :x.size(1)]
         return x
+    
+class RotaryPositionEmbedding(nn.Module):
+    def __init__(self, embed_dim):
+        pass
 
+    def forward(self, x):
+        pass
 
 class TransformerDecoder(nn.Module):
     # TODO: add flash attention
@@ -39,41 +45,63 @@ class TransformerDecoder(nn.Module):
                  n_blocks: int,
                  n_head: int,
                  ff_dim: int,
-                 text_dataset: TextDataset
+                 text_dataset: TextDataset,
+                 use_flash_attention: bool = False,
+                 use_rope: bool = False,
                  ) -> None:
         super().__init__()
+        self.use_flash_attention = use_flash_attention
         self.embeds = nn.Embedding(vocab_size, embed_dim)
-        self.pos_embeds = PositionalEncoding(embed_dim)
+        if use_rope:
+            self.pos_embeds = RotaryPositionEmbedding(embed_dim)
+        else:
+            self.pos_embeds = PositionalEncoding(embed_dim)
         self.decoder = nn.ModuleList([nn.TransformerEncoderLayer(embed_dim, n_head, ff_dim, batch_first=True) for _ in range(n_blocks)])
         self.fc = nn.Linear(embed_dim, vocab_size)
         self.softmax = nn.Softmax(dim=1)
 
         self.dataset = text_dataset
 
-    def forward(self, x, mask):
+
+    def _forward(self, x, mask):
         x = self.embeds(x)
         x = self.pos_embeds(x)
+
         for decoder_block in self.decoder:
             x = decoder_block(x, src_mask=mask, is_causal=True)
         x = self.fc(x)
         return x
 
 
-    def generate(self, prompt="", max_len=100):
+    def forward(self, x, mask):
+        if self.use_flash_attention:
+            with torch.backends.cuda.sdp_kernel(
+                enable_flash=True, 
+                enable_math=True, 
+                enable_mem_efficient=True
+            ):
+                return self._forward(x, mask)
+        return self._forward(x, mask)
+        
+
+    def generate(self, text_dataset, prompt="", max_len=100):
         # prompt initialization
-        input_ids = self.text_dataset.text2ids(prompt)
+        input_ids = text_dataset.text2ids(prompt)
         input_ids = torch.tensor(input_ids).unsqueeze(0)
+        # (1, seq_len, embed_dim)
 
-        memory = self.decoder.embedding(input_ids)
-        memory = self.decoder.positional_encoding(memory)
-
-        tgt_mask = torch.triu(torch.ones(max_len, max_len), diagonal=1).bool()
+        mask = torch.triu(torch.ones(max_len, max_len), diagonal=1).bool()
 
         for _ in range(max_len):
-            output = self.decoder(input_ids, memory, tgt_mask)
+            output = self.forward(input_ids, mask)
             output_probs = F.softmax(output[:, -1, :], dim=-1)
             next_token = torch.multinomial(output_probs, 1)
-            input_ids = torch.cat([input_ids, next_token.unsqueeze(1)], dim=-1)
+            input_ids = torch.cat([input_ids, next_token], dim=-1)
 
-        generated_text = self.text_dataset.ids2text(input_ids.squeeze().tolist())
+        generated_text = text_dataset.ids2text(input_ids.squeeze().tolist())
         return generated_text
+    
+
+# TODO: generation with beam search
+    def generate_with_beam_search(self, text_dataset, prompt="", max_len=100):
+        pass